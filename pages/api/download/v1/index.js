da8d9d31fbb808c48cc65504c1bb085658203646
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-04-09 03:19:29 -0300

    fix code style

--- pages/api/download/v1/index.js
+++ pages/api/download/v1/index.js
@@ -1,16 +1,13 @@
-import ytdl from "ytdl-core";
-import microCors from "micro-cors";
+import ytdl from 'ytdl-core'
+import microCors from 'micro-cors'
 
-const cors = microCors();
+const cors = microCors()
 
 async function Download(req, res) {
-  const { url } = req.query;
-  const info = await ytdl.getInfo(url);
-  res.setHeader(
-    "Content-disposition",
-    `attachment; filename=${info.title}.mp4`
-  );
-  ytdl(url, { filter: (format) => format.container === "mp4" }).pipe(res);
+  const { url } = req.query
+  const info = await ytdl.getInfo(url)
+  res.setHeader('Content-disposition', `attachment; filename=${info.title}.mp4`)
+  ytdl(url, { filter: (format) => format.container === 'mp4' }).pipe(res)
 }
 
-export default cors(Download);
+export default cors(Download)
